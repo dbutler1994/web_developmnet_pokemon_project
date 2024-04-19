9403c3e63ee43e730fa7f9129386d4d8b3ff9ae2
40103807 <dbutler04@qub.ac.uk>
2024-04-19 19:32:02 +0100

    Added media queries for card partial to get better control over sizing when on a single page view. Refactored wishlist button to only change opaciy if a cardId is passed in

--- webapp/public/js/wishlistButton.js
+++ webapp/public/js/wishlistButton.js
@@ -1,7 +1,8 @@
 
-const toggleHeartColour = (buttonId) => {
+const toggleHeartColour = (buttonId, cardGridImage) => {
     // Get the heart button
     const heartButton = document.getElementById(buttonId);
+    const cardImage = document.getElementById(cardGridImage);
 
     if (heartButton) {
         // search for the icon by looking for the i tag
@@ -10,6 +11,16 @@ const toggleHeartColour = (buttonId) => {
         // change colour if found using  tenary if statement
         if (heartIcon) {
             heartIcon.style.color = (heartIcon.style.color === 'red' ? 'grey' : 'red');
+        }
+
+                    
+        // remove or add opacity of the image if we have the image
+        if(cardImage){
+            if(cardImage.classList.contains('opacity-50')) {
+                cardImage.classList.remove('opacity-50');
+            } else {
+                cardImage.classList.add('opacity-50');
+            }
         }
     }
 };
