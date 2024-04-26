f37ef3dd5f4e5fb7e8e866b8c3d9749080f8dd77
40103807 <dbutler04@qub.ac.uk>
2024-04-26 04:30:48 +0100

    updated card getAllCards controller in webapp. Now picks up total count from the api and also logs and returns query parameters from the request and pushes to ejs templates

--- webapp/controllers/cards.js
+++ webapp/controllers/cards.js
@@ -5,8 +5,24 @@ const getAllCards = async (req, res) => {
     try {
         endPoint = 'http://localhost:4000/cards' + req.paramString;
         let response = await axios.get(endPoint);
-        let cardData = response.data;
-        res.render('cardGrid', { cards: cardData});
+        
+        // get card data and count data
+        let cardData = response.data.cardData;
+        let cardCount = response.data.summaryData.totalCards;
+
+        // get the cardsPerPage parameter value and page value from the request
+        let pageSize = res.req.query.cardsPerPage || 30;
+        let page = res.req.query.page || 1;
+
+        // render the card grid view
+        res.render('cardGrid', { 
+            cards: cardData, 
+            totalCards: cardCount, 
+            pageSize: pageSize, 
+            page: page
+        });
+
+
     } catch (error) {
         console.error('Error fetching card data:', error.message);
         res.status(500).render('error');
