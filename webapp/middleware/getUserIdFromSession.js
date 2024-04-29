b0011fdffc447af60952f89d506319fb1256b419
40103807 <dbutler04@qub.ac.uk>
2024-04-29 01:48:44 +0100

    Improvements to add user id to url function and get userid from session middleware

--- webapp/middleware/getUserIdFromSession.js
+++ webapp/middleware/getUserIdFromSession.js
@@ -1,7 +1,7 @@
 // middleware.js
 
 const getUserIdFromSession = (req, res, next) => {
-    if (req.session && req.session.user.account.id) {
+    if (req.session && req.session.user && req.session.user.account && req.session.user.account.id) {
       req.userId = req.session.user.account.id;
     }
     next();
